fac2e0c0c42fec0cbe8601996c3f60f609cc5ece
wuqui <q.wuerschinger@gmail.com>
2023-02-03 18:15:39 +0100

    add CL function for training models

--- redditdumps/_modidx.py
+++ redditdumps/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://wuqui.github.io',
                 'git_url': 'https://github.com/wuqui/redditdumps',
                 'lib_path': 'redditdumps'},
-  'syms': {'redditdumps.core': {'redditdumps.core.add_one': ('core.html#add_one', 'redditdumps/core.py')}}}
+  'syms': { 'redditdumps.core': { 'redditdumps.core.add_one': ('core.html#add_one', 'redditdumps/core.py'),
+                                  'redditdumps.core.get_docs': ('core.html#get_docs', 'redditdumps/core.py'),
+                                  'redditdumps.core.train_model': ('core.html#train_model', 'redditdumps/core.py')}}}
